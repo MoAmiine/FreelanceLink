a2f30f3c2b7d45c0368c37c57f1db6b1ee4a2572
Aymen Oumaalla <aymenoml2002@gmail.com>
2025-11-04 17:28:44 +0100

    `Added modal for profile modification and updated navbar scrolling functionality`

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,8 +1,23 @@
-  window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('header');
-    if (window.scrollY > 50) {
-      navbar.classList.add('scrolled');
-    } else {
-      navbar.classList.remove('scrolled');
-    }
-  });
+window.addEventListener('scroll', function() {
+  const navbar = document.querySelector('header');
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
+const modal = document.getElementById("loginModal");
+const openBtn = document.getElementById("openmodal");
+const closeBtn = document.getElementById("close");
+
+openBtn.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
+
+function openModal() {
+  modal.style.display = "flex";
+}
+
+function closeModal() {
+  modal.style.display = "none";
+}
