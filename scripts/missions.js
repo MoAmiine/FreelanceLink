bcb2e88cc5109b5f63a8c3ff235314404a2bb6c0
Moamiine <medamine.teljaoui@gmail.com>
2025-11-10 15:18:34 +0100

    page mission completed (non fonctionnel)

--- scripts/missions.js
+++ scripts/missions.js
@@ -1,25 +1,37 @@
-let page = document.getElementById('missions');
-fetch('/data/missions.json')
-    .then(response => response.json())
-    .then(missions => {
-        missions.forEach(mission => {
-            let card = document.createElement('div');
-            card.className = 'card';
-            card.style.width = '15rem';
-            card.style.margin = '10px';
-            card.style.display = 'inline-block';
-            card.style.textAlign = 'center';
-            card.style.padding = '10px';
-            card.style.boxShadow = '0 0 15px rgba(0, 0, 0, 0.47)';
-            card.style.borderRadius = '10px';
-            card.style.height = '250px';
-            card.style.lineHeight = '2.2rem';
-            card.innerHTML = `
+let page = document.getElementById("missions");
+let title = document.getElementById('missions-title');
+let cards = document.getElementById('card-group')
+title.style.fontWeight = 'bold'
+title.style.fontSize = '3rem'
+title.style.lineHeight = '10rem'
+page.style.textAlign = 'center';
+page.style.marginBottom = '5rem';
+page.style.marginTop = '1rem';
+page.style.display = 'flex';
+page.style.flexDirection = 'column';
+
+fetch("/data/missions.json")
+  .then((response) => response.json())
+  .then((missions) => {
+    missions.forEach((mission) => {
+      let card = document.createElement("div");
+      card.className = "card";
+      card.style.width = "30rem";
+      card.style.marginBottom = "3rem";
+      card.style.marginTop = "2rem"
+      card.style.display = "inline-block";
+      card.style.textAlign = "center";
+      card.style.padding = "10px";
+      card.style.boxShadow = "0 0 15px rgba(0, 0, 0, 0.47)";
+      card.style.borderRadius = "10px";
+      card.style.height = "250px";
+      card.style.lineHeight = "2.2rem";
+      card.innerHTML = `
                 <h3 class="mission-title">${mission.client}</h3>
                 <p class="mission-description">${mission.description}</p>
                 <p class="h3">${mission.prix} $</p>
-                <button class="btn-primary" >Postulez</button>
-            `;  
-            page.querySelector('.card-group').appendChild(card);
-        });
-    })
+                <button class="postulez">Postulez</button>
+            `;
+      page.querySelector(".card-group").appendChild(card);
+    });
+  });
